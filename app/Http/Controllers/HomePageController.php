6d651f1e225f6cc14498b6ac3532b7e2316941c3
marvin6863 <marvinwanyama69@gmail.com>
2023-09-15 14:17:45 +0300

    Making Hot News Section Dynamic

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class HomePageController extends Controller
 {
-    public function index () {
-        return view('front.home');
+    public function index()
+    {
+        $hot_news = Post::with('creator')
+            ->withCount('comments')
+            ->where('hot_news', 1)
+            ->where('status', 1)
+            ->orderBy('id', 'DESC')
+            ->first();
+        return view('front.home', compact('hot_news'));
     }
 }
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -8,33 +8,27 @@
                     <div class="feature_article_wrapper">
                         <div class="feature_article_img">
                             <img class="img-responsive top_static_article_img"
-                                src="{{ asset('front/assets/img/feature-top.jpg') }}" alt="feature-top">
+                                src="{{ asset('post') }}/{{ $hot_news->main_image }}" alt="{{ $hot_news->title }}">
                         </div>
                         <!-- feature_article_img -->
 
                         <div class="feature_article_inner">
                             <div class="tag_lg red"><a href="category.html">Hot News</a></div>
                             <div class="feature_article_title">
-                                <h1><a href="single.html" target="_self">Chevrolet car-saving technology
-                                        delivers </a></h1>
+                                <h1><a href="single.html" target="_self">{{ $hot_news->title}} </a></h1>
                             </div>
                             <!-- feature_article_title -->
 
-                            <div class="feature_article_date"><a href="#" target="_self">Stive
-                                    Clark</a>,<a href="#" target="_self">Aug
-                                    4, 2015</a></div>
+                            <div class="feature_article_date"><a href="#" target="_self">{{ $hot_news->creator->name}}</a>,<a href="#" target="_self">{{ date('j F -Y', strtotime($hot_news->created_at)) }}</a></div>
                             <!-- feature_article_date -->
 
                             <div class="feature_article_content">
-                                In a move to address mounting concerns about security on Android, Google and
-                                Samsung are
-                                now issuing.
+                                {{ $hot_news->short_description}}
                             </div>
                             <!-- feature_article_content -->
 
                             <div class="article_social">
-                                <span><i class="fa fa-share-alt"></i><a href="#">424</a>Shares</span>
-                                <span><i class="fa fa-comments-o"></i><a href="#">4</a>Comments</span>
+                                <span><i class="fa fa-comments-o"></i><a href="#">{{ $hot_news->comments_count }}</a>Comments</span>
                             </div>
                             <!-- article_social -->
 
